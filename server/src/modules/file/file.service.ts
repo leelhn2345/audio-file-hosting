a62abb3469dcc832b667bb004b298942b3b3e09d
Nelson Lee <leelhn2345@outlook.com>
2025-06-15 04:48:59 +0800

    throw error if object already exists

--- server/src/modules/file/file.service.ts
+++ server/src/modules/file/file.service.ts
@@ -1,6 +1,18 @@
 import { Buckets, minioClient, presignExpiryDuration } from "@config/minio.js";
 
+import { DuplicatedError } from "@errors/duplicate.js";
+
 import { UserSessionType } from "@modules/user/user.schema.js";
+
+async function checkIfObjectExists(bucket: Buckets, objectKey: string) {
+  try {
+    await minioClient.statObject(bucket, objectKey);
+    return true;
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (_) {
+    return false;
+  }
+}
 
 export async function postPresignedUrl(
   bucket: Buckets,
@@ -9,6 +21,14 @@ export async function postPresignedUrl(
 ) {
   const userId = user.id;
   const objectKey = `${userId}/${fileName}`;
+  const exists = await checkIfObjectExists(bucket, objectKey);
+
+  if (exists) {
+    throw new DuplicatedError(
+      `An item with the same name already exists. Please rename filename.`,
+    );
+  }
+
   const presignedUrl = await minioClient.presignedPutObject(
     bucket,
     objectKey,
