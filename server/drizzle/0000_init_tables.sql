ade9d5e3f94aaec28732cd1c9de78b594a8c566a
Nelson Lee <leelhn2345@outlook.com>
2025-06-15 00:46:47 +0800

    sql migration file

--- server/drizzle/0000_init_tables.sql
+++ server/drizzle/0000_init_tables.sql
@@ -3,7 +3,7 @@ CREATE TABLE "users" (
 	"name" text NOT NULL,
 	"email" text NOT NULL,
 	"password" text NOT NULL,
-	"created_at" timestamp with time zone NOT NULL,
-	"updated_at" timestamp with time zone NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"updated_at" timestamp with time zone DEFAULT now() NOT NULL,
 	CONSTRAINT "users_email_unique" UNIQUE("email")
 );
