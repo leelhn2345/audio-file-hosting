3f71f97aee7ffdfcc52aec3300d36da1b2886161
Nelson Lee <leelhn2345@outlook.com>
2025-06-15 03:56:05 +0800

    audio and genre tables

--- server/src/db/tables/audio.table.ts
+++ server/src/db/tables/audio.table.ts
@@ -1,7 +1,16 @@
-import { pgTable, timestamp, uuid } from "drizzle-orm/pg-core";
+import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+
+import { userTable } from "./user.table.js";
 
 export const audioTable = pgTable("audios", {
   id: uuid().primaryKey(),
+  name: text().notNull(),
+  description: text(),
+  artist: text(),
+  releaseDate: timestamp({ withTimezone: true, mode: "string" }),
+  uploadedBy: uuid()
+    .references(() => userTable.id, { onDelete: "cascade" })
+    .notNull(),
   createdAt: timestamp({ withTimezone: true, mode: "string" })
     .notNull()
     .defaultNow(),
