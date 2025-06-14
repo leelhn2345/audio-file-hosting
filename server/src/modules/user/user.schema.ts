2b4a58526d90e79bd5b5fbf26ba4f7c63e243ced
Nelson Lee <leelhn2345@outlook.com>
2025-06-15 01:26:20 +0800

    user schema

--- server/src/modules/user/user.schema.ts
+++ server/src/modules/user/user.schema.ts
@@ -1,7 +1,15 @@
-import { Type as t } from "@sinclair/typebox";
+import { Static, Type as t } from "@sinclair/typebox";
 import { createInsertSchema } from "drizzle-typebox";
 
 import { userTable } from "@db/tables/user.table.js";
+
+export const UserSessionSchema = t.Object({
+  id: t.String({ format: "uuid" }),
+  email: t.String({ format: "email" }),
+  name: t.String(),
+});
+
+export type UserSessionType = Static<typeof UserSessionSchema>;
 
 const UserTableSchema = createInsertSchema(userTable);
 
