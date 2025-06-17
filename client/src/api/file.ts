6d87ab3b677b7c2bf163633cb85439800d111e5c
Nelson Lee <leelhn2345@outlook.com>
2025-06-17 22:01:45 +0800

    file apis

---
+++ client/src/api/file.ts
@@ -0,0 +1,45 @@
+export enum Bucket {
+  AUDIO = "audio",
+}
+
+export type FileObject = {
+  bucket: Bucket;
+  objectKey: string;
+};
+
+export async function generateUploadUrl(fileObject: FileObject) {
+  const res = await fetch(
+    `${import.meta.env.VITE_BACKEND_URL}/file/upload-url`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify(fileObject),
+    },
+  );
+  const result = await res.json();
+  if (!res.ok) throw new Error(result.message);
+  const url: string = result.presignedUrl;
+  return url;
+}
+
+export async function generateDownloadUrl(fileObject: FileObject) {
+  const searchParams = new URLSearchParams(fileObject);
+
+  const res = await fetch(
+    `${import.meta.env.VITE_BACKEND_URL}/file/download-url?${searchParams}`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    },
+  );
+
+  const result = await res.json();
+  if (!res.ok) throw new Error(result.message);
+  const url: string = result.presignedUrl;
+  return url;
+}
