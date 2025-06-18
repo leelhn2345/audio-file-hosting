e39c8e2fddfd85aa86077b68251ea5db56f5f83d
Nelson Lee <leelhn2345@outlook.com>
2025-06-18 19:25:01 +0800

    sql migration

--- server/src/db/tables/genre.table.ts
+++ server/src/db/tables/genre.table.ts
@@ -1,11 +1,17 @@
-import { pgTable, text, uuid } from "drizzle-orm/pg-core";
+import { pgTable, text, uniqueIndex, uuid } from "drizzle-orm/pg-core";
 
 import { userTable } from "./user.table.js";
 
-export const genreTable = pgTable("genres", {
-  id: uuid().primaryKey(),
-  name: text().notNull(),
-  userId: uuid()
-    .references(() => userTable.id, { onDelete: "cascade" })
-    .notNull(),
-});
+export const genreTable = pgTable(
+  "genres",
+  {
+    id: uuid().primaryKey(),
+    name: text().notNull(),
+    userId: uuid()
+      .references(() => userTable.id, { onDelete: "cascade" })
+      .notNull(),
+  },
+  (table) => {
+    return [uniqueIndex("uniqueUserGenre").on(table.name, table.userId)];
+  },
+);
