077af6b8920b59534cff397fb5ce999d0fa4ed1c
Nelson Lee <leelhn2345@outlook.com>
2025-06-18 19:25:06 +0800

    genre router

--- server/src/modules/genre/genre.schema.ts
+++ server/src/modules/genre/genre.schema.ts
@@ -1,6 +1,10 @@
 import { Type as t } from "@sinclair/typebox";
+import { createInsertSchema } from "drizzle-typebox";
+
+import { genreTable } from "@db/tables/genre.table.js";
 
 import { PaginationSchema } from "@utils/pagination.js";
+import { allDataSchemaExtender } from "@utils/schema.js";
 
 export const GenrePaginationSchema = t.Composite([
   PaginationSchema,
@@ -10,3 +14,7 @@ export const GenrePaginationSchema = t.Composite([
     ),
   }),
 ]);
+
+export const GenreTableSchema = createInsertSchema(genreTable);
+
+export const AllGenreSchema = allDataSchemaExtender(GenreTableSchema);
