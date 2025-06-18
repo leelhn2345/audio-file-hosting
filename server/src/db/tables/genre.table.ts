28bc00aca1225894eae5961cd8d461489f8037c7
Nelson Lee <leelhn2345@outlook.com>
2025-06-18 19:41:02 +0800

    add metadata

--- server/src/db/tables/genre.table.ts
+++ server/src/db/tables/genre.table.ts
@@ -1,4 +1,10 @@
-import { pgTable, text, uniqueIndex, uuid } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  text,
+  timestamp,
+  uniqueIndex,
+  uuid,
+} from "drizzle-orm/pg-core";
 
 import { userTable } from "./user.table.js";
 
@@ -9,6 +15,12 @@ export const genreTable = pgTable(
     name: text().notNull(),
     userId: uuid()
       .references(() => userTable.id, { onDelete: "cascade" })
+      .notNull(),
+    createdAt: timestamp({ withTimezone: true, mode: "string" })
+      .defaultNow()
+      .notNull(),
+    updatedAt: timestamp({ withTimezone: true, mode: "string" })
+      .defaultNow()
       .notNull(),
   },
   (table) => {
